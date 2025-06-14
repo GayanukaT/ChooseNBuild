6c86e9f6889ca327e5e5eb8e9f6a5084d5bad850
Rytuoo <yasarathennakoon83@gmail.com>
2025-06-14 14:12:46 +0530

    update index file for authentication

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,51 @@
-const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
-app.get('/',(req, res) => {
-    res.send('Welcome to ChooseNBuild Backend!');
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('ChooseNBuild API is running');
 });
 
-app.listen(postMessage, () => {
-    console.log('Server is running on http://localhost:${PORT}');
+// TODO: Mount routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+nst express = require('express');
+const cors = require('cors');
+consuyht dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+g
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Example route
+app.get('/', (req, res) => {
+  res.send('ChooseNBuild API is running');
+});
+
+// TODO: Mount routes
+// app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
