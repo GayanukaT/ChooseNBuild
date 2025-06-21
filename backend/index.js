45d6f4c6169873c3ff9b4dfa30d9af96b34f7151
ishananjana <hmanjana2001@gmail.com>
2025-06-21 11:00:08 +0530

    fixed some errors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,57 +1,25 @@
+const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 
 dotenv.config();
+
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// Middleware
 
-// Connect to MongoDB
-connectDB();
+// Connect to mongoDB
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => console.log("Mongo DB connected"))
+.catch((err) => console.log("Mongo DB error", err));
 
-// Example route
-app.get('/', (req, res) => {
-  res.send('ChooseNBuild API is running');
-});
+// Routes
 
-// TODO: Mount routes
-// app.use('/api/auth', require('./routes/authRoutes'));
 
+// Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
-nst express = require('express');
-const cors = require('cors');
-consuyht dotenv = require('dotenv');
-const connectDB = require('./config/db');
-
-dotenv.config();
-const app = express();
-g
-app.use(cors());
-app.use(express.json());
-
-// Connect to MongoDB
-connectDB();
-
-// Example route
-app.get('/', (req, res) => {
-  res.send('ChooseNBuild API is running');
-});
-
-// TODO: Mount routes
-// app.use('/api/auth', require('./routes/authRoutes'));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-const projectRoutes = require('./routes/projectRoutes');
-app.use('api/projects', projectRoutes);
-
-const bidRoutes = require('./routes/bidRoutes');
-app.use('/api/bids', bidRoutes);
